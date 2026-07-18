a9effe2d4f217a50a0ef313d882f6395a92e9a35
MOHAMMAD AMEENUDDIN <maameen862@gmail.com>
2026-07-19 02:21:35 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,30 @@
-return (
-  <main id="main" className="min-h-screen">
-    <Navbar />
-    <Hero />
-    <About />
-    <Skills />
-    <Experience />
-    <Projects />
-    <CustomSections />
-    <Gallery />
-    <Posts />
-    <Contact />
-    <Footer />
-  </main>
-);
+import { Navbar } from "@/components/Navbar";
+import { Hero } from "@/components/Hero";
+import { About } from "@/components/About";
+import { Skills } from "@/components/Skills";
+import { Experience } from "@/components/Experience";
+import { Projects } from "@/components/Projects";
+import { CustomSections } from "@/components/CustomSections";
+import { Gallery } from "@/components/Gallery";
+import { Posts } from "@/components/Posts";
+import { Contact, Footer } from "@/components/Contact";
+
+function Home() {
+  return (
+    <main id="main" className="min-h-screen">
+      <Navbar />
+      <Hero />
+      <About />
+      <Skills />
+      <Experience />
+      <Projects />
+      <CustomSections />
+      <Gallery />
+      <Posts />
+      <Contact />
+      <Footer />
+    </main>
+  );
+}
+
+export default Home;
